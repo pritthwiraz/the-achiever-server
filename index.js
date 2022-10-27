515fc44e0244f338f03375f70672b027a31a1f57
pritthwiraz <pritthwiraz@gmail.com>
2022-10-27 22:35:44 +0600

    updeted data

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
+const courses = require('./Data/Courses.json');
+
 app.get('/', (req, res) =>{
     res.send('Courses API Running')
+})
+
+app.get('/courses', (req, res) =>{
+    res.send(courses)
 })
 
 app.listen(port, () =>{
